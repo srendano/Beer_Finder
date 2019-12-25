4a2b3db694841020e3927e438867af0292824a6a
Spencer Rendano <srendano@umass.edu>
2019-12-25 18:38:33 -0500

    New Commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import config from './config'
 import express from 'express'; 
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Fuck You Ryan')
+    res.send('Hello World')
 })
 app.listen(config().port, () => {
   console.log(`Listening on port ${config().port}`);
